ddd123a173742e7e7d0512e8122364fc0d9d4485
Francois Beutin <francois.beutin@ledger.fr>
2023-03-01 15:37:27 +0100

    Use a dedicated module for logging, use colors

--- scripts/check_makefile.sh
+++ scripts/check_makefile.sh
@@ -2,47 +2,48 @@
 
 set -e
 
+# shellcheck source=scripts/logger.sh
+source "$(dirname "$0")/logger.sh"
+
 main() (
+    error=0
     repo="$1"
     repo_name="$2"
-
-    error=0
 
     # Find the main Makefile of the app
     makefile=$(grep -Rl --include="*Makefile*" "^[[:blank:]]*APPNAME" "$repo")
 
     if ! grep -q "^listvariants:" "$makefile"; then
-        echo "ERROR => The Makefile does not contain the 'listvariants' rule"
+        log_error "The Makefile does not contain the 'listvariants' rule"
         error=1
     fi
 
     if echo "$repo_name" | grep -q "app-boilerplate"; then
-        echo "APPNAME and VARIANT checks skipped for Boilerplate"
+        log_warning "APPNAME and VARIANT checks skipped for Boilerplate"
     else
         while IFS= read -r line; do
             if echo "$line" | grep -q -i "boilerplate"; then
-                echo "ERROR => APPNAME should refer to your application's name, not boilerplate"
+                log_error "APPNAME should refer to your application's name, not boilerplate"
                 error=1
             fi
         done < <(grep "^[[:blank:]]*APPNAME" "$makefile")
 
         if grep "echo VARIANTS" "$makefile" | grep -q "BOL"; then
-            echo "ERROR => VARIANT name should refer to your coin ticker, not boilerplate's BOL"
+            log_error "VARIANT name should refer to your coin ticker, not boilerplate's BOL"
             error=1
         fi
     fi
 
     if grep -q "HAVE_BOLOS_UX" "$makefile"; then
-        echo "ERROR => The Makefile contains an outdated flag 'HAVE_BOLOS_UX'"
+        log_error "The Makefile contains an outdated flag 'HAVE_BOLOS_UX'"
         error=1
     fi
 
     if [[ error -eq 0 ]]; then
-        echo "SUCCESS => Makefile \"$makefile\" is compliant"
+        log_success "Makefile \"$makefile\" is compliant"
     else
-        echo "FAILURE =>"
-        echo "cat \"$makefile\""
-        cat "$makefile"
+        log_error_no_header "At least one error has been found"
+        log_error_no_header "To check the Makefile content, run \"cat '$makefile'\""
     fi
 
     return "$error"
--- scripts/check_readme.sh
+++ scripts/check_readme.sh
@@ -2,36 +2,38 @@
 
 set -e
 
+# shellcheck source=scripts/logger.sh
+source "$(dirname "$0")/logger.sh"
+
 main() (
+    error=0
     repo="$1"
     repo_name="$2"
-    error=0
 
     if [[ ! -f "$repo/README.md" ]]; then
-        echo "ERROR => No README.md found in your project"
+        log_error "No README.md found in your project"
         error=1
     fi
 
     if [[ ! -s "$repo/README.md" ]]; then
-        echo "ERROR => The README.md must not be empty"
+        log_error "The README.md must not be empty"
         error=1
     fi
 
     if echo "$repo_name" | grep -q "app-boilerplate"; then
-        echo "Readme check skipped for Boilerplate"
+        log_warning "Readme check skipped for Boilerplate"
     else
         if grep -q -i "^#.*boilerplate" "$repo/README.md"; then
-            echo "ERROR => The README.md should mention your application, not boilerplate"
+            log_error "The README.md should mention your application, not boilerplate"
             error=1
         fi
     fi
 
     if [[ error -eq 0 ]]; then
-        echo "SUCCESS => Readme customization is sufficient"
+        log_success "Readme customization is sufficient"
     else
-        echo "FAILURE =>"
-        echo "cat \"$repo/README.md\""
-        cat "$repo/README.md"
+        log_error_no_header "At least one error has been found"
+        log_error_no_header "To check the Readme content, run \"cat '$repo/README.md'\""
     fi
 
     return "$error"
