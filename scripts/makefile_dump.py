34e06ffb8a8a08bc70176653f7b453e69b0ea396
Xavier Chapron <xavier.chapron@ledger.fr>
2023-09-19 15:43:07 +0200

    Adapt reusable workflows for Rust apps

--- scripts/makefile_dump.py
+++ scripts/makefile_dump.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import subprocess
 from argparse import ArgumentParser
 from pathlib import Path
 from typing import Tuple, List, Dict
 from tempfile import NamedTemporaryFile
 import json
+from utils import run_cmd
 
 
 LISTPARAMS_MAKEFILE = """
@@ -25,30 +25,6 @@ listparams:
 \t@echo SDK_HASH=$(SDK_HASH)
 \t@echo Stop dumping params
 """
-
-
-def run_cmd(cmd: str,
-            cwd: Path,
-            print_output: bool = False,
-            no_throw: bool = False) -> str:
-    print(f"[run_cmd] Running: {cmd} from {cwd}")
-
-    ret = subprocess.run(cmd,
-                         shell=True,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT,
-                         universal_newlines=True,
-                         cwd=cwd)
-    if no_throw is False and ret.returncode:
-        print(f"[run_cmd] Error {ret.returncode} raised while running cmd: {cmd}")
-        print("[run_cmd] Output was:")
-        print(ret.stdout)
-        raise ValueError()
-
-    if print_output:
-        print(f"[run_cmd] Output:\n{ret.stdout}")
-
-    return ret.stdout.strip()
 
 
 def get_app_listvariants(app_build_path: Path) -> Tuple[str, List[str]]:
