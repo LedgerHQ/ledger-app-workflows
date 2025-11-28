be88408c8e142fab00fe7bad390684185bcdd30b
GroM <jerome.caporossi@ledger.fr>
2025-11-28 11:50:45 +0100

    remove useless for loop

--- scripts/check_rust_sdk.sh
+++ scripts/check_rust_sdk.sh
@@ -26,20 +26,13 @@ if [ -n "$RUST_NIGHTLY" ]; then
         exit 1
     fi
 fi
-# Get the list of dependencies and filter the Rust SDK crates
-all_crates=$(cargo tree --depth 2 --prefix none | awk '{print $1}' | sort -u)
-for crate in "ledger_device_sdk"; do
-    if echo "$all_crates" | grep -q "^$crate$"; then
-        curr_version=$(cargo tree -p $crate | grep -oE "$crate v[0-9]+\.[0-9]+\.[0-9]+" | awk '{print $2}' | sed 's/^v//')
-        latest_version=$(cargo search $crate --limit 1 | grep -oE "$crate = \"[0-9]+\.[0-9]+\.[0-9]+\"" | awk -F '"' '{print $2}')
-        if [[ "${latest_version}" != "${curr_version}" ]]; then
-            log_error "$crate : version is $curr_version whereas $latest_version is available; please update!"
-            log_error "Application is always built with the latest versions of the Rust SDK crates"
-            log_error "Don't forget to activate dependabot in your repository settings to automatically update the Rust SDK dependencies"
-            log_error "see https://github.com/LedgerHQ/app-boilerplate-rust/blob/main/.github/dependabot.yml"
-            exit 1
-        fi
-    fi
-done
-
-
+# Check if Rust SDK crate is last version
+curr_version=$(cargo tree -p ledger_device_sdk | grep -oE "ledger_device_sdk v[0-9]+\.[0-9]+\.[0-9]+" | awk '{print $2}' | sed 's/^v//')
+latest_version=$(cargo search ledger_device_sdk --limit 1 | grep -oE "ledger_device_sdk = \"[0-9]+\.[0-9]+\.[0-9]+\"" | awk -F '"' '{print $2}')
+if [[ "${latest_version}" != "${curr_version}" ]]; then
+    log_error "ledger_device_sdk : version is $curr_version whereas $latest_version is available; please update!"
+    log_error "Application is always built with the latest versions of the Rust SDK crate"
+    log_error "Don't forget to activate dependabot in your repository settings to automatically update the Rust SDK dependencies"
+    log_error "see https://github.com/LedgerHQ/app-boilerplate-rust/blob/main/.github/dependabot.yml"
+    exit 1
+fi
